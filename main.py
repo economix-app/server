50d6a39a18be9a0b9359244219aeed9cec44d4f3
proplayer919 <95329795+proplayer919@users.noreply.github.com>
2025-03-28 18:04:19 +1100

    fix

--- main.py
+++ main.py
@@ -307,8 +307,32 @@ def update_item(item_id: str):
 
 def update_pet(pet_id: str):
     pet = Collections['pets'].find_one({"id": pet_id})
-    if not pet or not pet["alive"]:
+    if not pet:
         return
+      
+    if "history" not in pet:
+        pet["history"] = []
+    
+    if "last_fed" not in pet:
+        pet["last_fed"] = int(time.time())
+        
+    if "health" not in pet:
+        pet["health"] = "healthy"
+        
+    if "alive" not in pet:
+        pet["alive"] = True
+        
+    if "level" not in pet:
+        pet["level"] = 1
+        
+    if "exp" not in pet:
+        pet["exp"] = 0
+        
+    if "benefits" not in pet:
+        pet["benefits"] = {"token_bonus": 1}
+        
+    if "base_price" not in pet:
+        pet["base_price"] = 100
 
     last_fed = pet["last_fed"]
     now = int(time.time())
