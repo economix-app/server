0a89431e1310fb6d18ff2f7b5e50330e7881cc2e
proplayer919 <95329795+proplayer919@users.noreply.github.com>
2025-04-29 16:24:19 +1000

    fix

--- main.py
+++ main.py
@@ -3599,6 +3599,18 @@ def get_downtime():
     downtime = Collections["misc"].find_one({"type": "downtime"})
     if not downtime:
         return jsonify({"downtime": False})
+      
+    user = Collections["users"].find_one({"username": request.username})
+    if not user:
+        return jsonify({"error": "User not found", "code": "user-not-found"}), 404
+      
+    if user.get("type") == "admin":
+        return jsonify(
+            {
+                "downtime": False,
+                "message": "",
+            }
+        )
 
     return jsonify(
         {
