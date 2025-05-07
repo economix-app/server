1c6164ec2b046ded423595aaee8b071e3b2302bf
proplayer919 <95329795+proplayer919@users.noreply.github.com>
2025-05-07 18:01:26 +1000

    change staff badge colour

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ COSMETICS = {
 BADGES = {
     "staff": {
         "name": "Staff",
-        "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-tools"><path stroke="#18CB96" d="M3 21h4L20 8a1.5 1.5 0 0 0-4-4L3 17zM14.5 5.5l4 4"/><path stroke="#18CB96" d="M12 8 7 3 3 7l5 5M7 8 5.5 9.5M16 12l5 5-4 4-5-5m4 1-1.5 1.5"/></svg>',
+        "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-tools"><path stroke="#9c84ef" d="M3 21h4L20 8a1.5 1.5 0 0 0-4-4L3 17zM14.5 5.5l4 4"/><path stroke="#9c84ef" d="M12 8 7 3 3 7l5 5M7 8 5.5 9.5M16 12l5 5-4 4-5-5m4 1-1.5 1.5"/></svg>',
     },
     "shield": {
         "name": "Moderator",
