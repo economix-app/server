832087a6cb4432ebe58a4116a408d6df970939b6
proplayer919 <95329795+proplayer919@users.noreply.github.com>
2025-05-07 18:35:14 +1000

    update

--- main.py
+++ main.py
@@ -260,11 +260,7 @@ BADGES = {
     "proplus": {
         "name": "PRO+",
         "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36"><path fill="#FFAC33" d="M28.84 17.638c-.987 1.044-1.633 3.067-1.438 4.493l.892 6.441c.197 1.427-.701 2.087-1.996 1.469l-5.851-2.796c-1.295-.62-3.408-.611-4.7.018l-5.826 2.842c-1.291.629-2.193-.026-2.007-1.452l.843-6.449c.186-1.427-.475-3.444-1.47-4.481l-4.494-4.688c-.996-1.037-.655-2.102.755-2.365l6.37-1.188c1.41-.263 3.116-1.518 3.793-2.789L16.762.956c.675-1.271 1.789-1.274 2.473-.009L22.33 6.66c.686 1.265 2.4 2.507 3.814 2.758l6.378 1.141c1.412.252 1.761 1.314.774 2.359z"/><path fill="#FFD983" d="M9.783 2.181c1.023 1.413 2.446 4.917 1.717 5.447-.728.531-3.607-1.91-4.63-3.323s-.935-2.668-.131-3.254c.804-.587 2.02-.282 3.044 1.13m19.348 2.124C28.109 5.718 25.23 8.16 24.5 7.627c-.729-.53.695-4.033 1.719-5.445C27.242.768 28.457.463 29.262 1.051c.803.586.89 1.841-.131 3.254M16.625 33.291c-.001-1.746.898-5.421 1.801-5.421.897 0 1.798 3.675 1.797 5.42 0 1.747-.804 2.712-1.8 2.71-.994.002-1.798-.962-1.798-2.709m16.179-9.262c-1.655-.539-4.858-2.533-4.579-3.395.277-.858 4.037-.581 5.69-.041 1.655.54 2.321 1.605 2.013 2.556-.308.95-1.469 1.42-3.124.88M2.083 20.594c1.655-.54 5.414-.817 5.694.044.276.857-2.928 2.854-4.581 3.392-1.654.54-2.818.07-3.123-.88-.308-.95.354-2.015 2.01-2.556"/></svg>',
-    },
-    "system": {
-        "name": "System",
-        "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-settings"><path stroke="#F47B67" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37 1 .608 2.296.07 2.572-1.065"/><path stroke="#F47B67" d="M9 12a3 3 0 1 0 6 0 3 3 0 0 0-6 0"/></svg>',
-    },
+    }
 }
 
 EARLY_SUPPORTER_DEADLINE = 1767186000
@@ -569,7 +565,6 @@ def send_discord_notification(title: str, description: str, color: int = 0x00FF0
                 "message": description,
                 "timestamp": int(time.time()),
                 "type": "system",
-                "badges": [BADGES["system"]],
             }
         )
 
@@ -1217,7 +1212,6 @@ def register(username: str, password: str, ip: str) -> Tuple[dict, int]:
             <p>IP: <b>{ip}</b> has been blocked for <b>{AUTOMOD_CONFIG['ACCOUNT_CREATION_BLOCK_DURATION']} seconds</b></p>
             """,
                 "timestamp": current_time,
-                "badges": [BADGES["system"]],
                 "type": "system",
             }
         )
@@ -1655,7 +1649,6 @@ def send_message(
                     f"<p>User: <b>{username}</b> has been muted for <b>{mute_duration}</b></p>"
                 ),
                 "timestamp": current_time,
-                "badges": [BADGES["system"]],
                 "type": "system",
             }
         )
@@ -1681,9 +1674,6 @@ def send_message(
                     "username": "Command Handler",
                     "message": system_message,
                     "timestamp": time.time(),
-                    "badges": [
-                        BADGES["system"],
-                    ],
                     "type": "system",
                     "visibility": [username],
                 }
